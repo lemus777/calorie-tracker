9d02bb91e6013cf824bb04f44b2a370f829e47ab
lemus777 <lemus777@gmail.com>
2024-05-29 14:15:42 +0200

    data input form added

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,52 @@
+import { categories } from "../data/categories"
+
 export default function Form() {
   return (
-    <div>Form</div>
+    <form
+      className="space-y-5 bg-white shadow p-10 rounded-lg"
+    >
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="category">Categoría:</label>
+        <select
+          className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+          id="category"
+        >
+          {categories.map(category => (
+            <option
+              key={category.id}
+              value={category.id}
+            >
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="activity">Actividad:</label>
+        <input
+          id="activity"
+          type="text"
+          className="border border-slate-300 p-2 rounded-lg"
+          placeholder="Ej. comida, zumo de naranja, ensalada, ejercicio, pesas, bicicleta"
+        />
+      </div>
+
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="calories">Calorías:</label>
+        <input
+          id="calories"
+          type="number"
+          className="border border-slate-300 p-2 rounded-lg"
+          placeholder="Calorias, ej. 300 o 500"
+        />
+      </div>
+
+      <input
+        type="submit"
+        className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
+        value='Guardar comida o guardar ejercicio'
+      />
+    </form>
   )
 }
