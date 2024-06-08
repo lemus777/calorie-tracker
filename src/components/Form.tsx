c0e6666860035165b4c2008a6d28c93642875d0a
lemus <lemus777@gmail.com>
2024-06-08 18:01:00 +0200

    reducer added

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,13 @@
-import { useState, ChangeEvent } from "react"
-import { Activity } from "../types"
+import { useState, ChangeEvent, FormEvent, Dispatch } from "react"
+import type { Activity } from "../types"
 import { categories } from "../data/categories"
+import { ActivityActions } from "../reducers/activity-reducer"
 
-export default function Form() {
+type FormProps = {
+  dispatch: Dispatch<ActivityActions>
+}
+
+export default function Form({dispatch}: FormProps) {
 
   const [activity, setActivity] = useState<Activity>({
     category: 1,
@@ -20,13 +25,18 @@ export default function Form() {
 
   const isValidActivity = () => {
     const { name, calories } = activity
-    console.log(name.trim() !== '' && calories > 0)
     return name.trim() !== '' && calories > 0
+  }
+
+  const handleSubmit = (e : FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    dispatch({ type: 'save-activity', payload: {newActivity: activity}})
   }
   
   return (
     <form
       className="space-y-5 bg-white shadow p-10 rounded-lg"
+      onSubmit={handleSubmit}
     >
       <div className="grid grid-cols-1 gap-3">
         <label htmlFor="category" className="font-bold">Categoría:</label>
@@ -74,7 +84,7 @@ export default function Form() {
       <input
         type="submit"
         className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer disabled:opacity-10"
-        value='Guardar comida o guardar ejercicio'
+        value={activity.category === 1 ? 'Guardar comida' : 'guardar ejercicio'}
         disabled={!isValidActivity()}
       />
     </form>
