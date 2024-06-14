777680fbfd6e85970b7be28df98dd46c342040cd
lemus <lemus777@gmail.com>
2024-06-14 12:57:43 +0200

    delete activity implemented

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,7 +1,7 @@
 import { Dispatch, useMemo } from "react"
 import { Activity } from "../types"
 import { categories } from "../data/categories"
-import { PencilSquareIcon } from "@heroicons/react/24/outline"
+import { PencilSquareIcon, XCircleIcon } from "@heroicons/react/24/outline"
 import { ActivityActions } from "../reducers/activity-reducer"
 
 type ActivityListProps = {
@@ -33,6 +33,14 @@ export default function ActivityList({activities, dispatch} : ActivityListProps)
             >
               <PencilSquareIcon
                 className="h-8 w-8 text-gray-800"
+              />
+            </button>
+
+            <button
+              onClick={() => dispatch({type: 'delete-activity', payload: {id: activity.id}})}
+            >
+              <XCircleIcon
+                className="h-8 w-8 text-red-500"
               />
             </button>
           </div>
